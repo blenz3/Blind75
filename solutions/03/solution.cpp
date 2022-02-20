f80ee6ac68942e5f1d507dc47fdcc2843415bb0a
Braden Lenz <16659708+blenz3@users.noreply.github.com>
2022-02-20 12:57:45 -0800

    Update solution.cpp

--- solutions/03/solution.cpp
+++ solutions/03/solution.cpp
@@ -1,40 +1,63 @@
 class Solution {
 public:
 #if 0
-    // Brute Force
-    //  - Linear search for the minimum value and return it
-    //    the array has been rotated
-    //  - Time linear
+    // Brute force 
+    //  - Check each pair to see if any show up twice
+    //  - Time quadratic
     //  - Space constant
-    int findMin(vector<int>& nums) {
-        int min = INT_MAX;
+    bool containsDuplicate(vector<int>& nums) {
         for (int i = 0; i < nums.size(); ++i) {
-            min = std::min(min, nums[i]);
-        }
-        return min;
-    }
-#endif
-    
-    // Optimized 
-    //  - Binary search 
-    //  - Time lgn
-    //  - Space const
-    int findMin(vector<int>& nums) {
-        int begin = 0, end = nums.size() - 1, min = INT_MAX;
-        while (begin <= end) {
-            
-            int mid = (begin + end) / 2;
-            min = std::min(nums[mid], min);
-            // If the midpoint is less than the earlier bound we need to keep searching left
-            if (nums[mid] < nums[begin]) {
-                min = std::min(nums[end], min);
-                end = mid - 1;
-            } else {
-                min = std::min(nums[begin], min);
-                begin = mid + 1;
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
             }
         }
         
-        return min;
+        return false;
+    }
+#endif
+    
+#if 0
+    // Optimized for time
+    //  - Insert elements to hash set check if sizes are equal
+    //  - Time linear
+    //  - Space linear
+    bool containsDuplicate(vector<int>& nums) {
+        std::unordered_set<int> numbers_encountered(nums.begin(), nums.end());
+        return (numbers_encountered.size() != nums.size());
+    }
+#endif
+
+#if 0
+    // Optimized for time + 1
+    //  - Insert elements to hash set check if sizes are equal
+    //  - Time linear
+    //  - Space linear
+    bool containsDuplicate(vector<int>& nums) {
+        std::unordered_set<int> numbers_encountered;
+        for (int num : nums) {
+            auto find_it = numbers_encountered.find(num);
+            if (find_it != numbers_encountered.end())
+                return true;
+            
+            numbers_encountered.insert(num);
+        }
+        
+        return false;
+    }
+#endif
+    
+    // Optimized for space
+    //  - Sort in place then walk the list comparing pair wise
+    //  - Time nlgn
+    //  - Space const
+    bool containsDuplicate(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == nums[i + 1]) 
+                return true;
+        }
+        return false;
     }
 };
